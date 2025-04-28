320ffb277e5d896de81a288fa15215b00e498957
darksprizor <47464148+darksprizor@users.noreply.github.com>
2025-04-28 17:03:45 -0500

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Darksprizor's Guide to The 7 Seas",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
